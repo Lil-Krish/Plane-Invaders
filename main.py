0eb85855807e6bb3a20ba33ce8e76a993c85b5bc
Lil-Krish <kcbhatraju@gmail.com>
2020-08-01 19:28:02 -0400

    Deleted menu and integrated images
    Menu was deleted in this commit but will be added in a later commit. Images have been integrated into main.py.

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import sys
 import pygame
 import pygame_menu
+import time
+import random
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+
+rs = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+bs  = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+gs = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+
+ys = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+rl = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+bl = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+gl = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+
+yl = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+bg = pygame.image.load(os.path.join("assets", "background-black.png"))
+
+width, height = 750, 750
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Game Jam 1')
 
 running = True
@@ -25,10 +43,25 @@ def play_level_1():
             if event.type == pygame.QUIT:
                 quit_screen()
 
+def main():
+    global menu_show
 
-menu = pygame_menu.Menu(300, 400, 'Space Invaders', theme=pygame_menu.themes.THEME_BLUE)
-menu.add_text_input('Name: ', default='John Doe')
-menu.add_button('Play', play_level_1)
-menu.add_button('Quit', quit_screen)
+    run = True
+    FPS = 90
+    clock = pygame.time.Clock()
 
-menu.mainloop(screen)
+    def reDraw():
+        screen.blit(bg, (0,0))
+
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)
+        reDraw()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                quit_screen()
+
+main()
