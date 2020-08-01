dab2467632498f82174a7dead3a5762a9e948e88
Lil-Krish <kcbhatraju@gmail.com>
2020-08-01 17:35:39 -0400

    Added Main Menu
    Created Main Menu initialized with pygame-menu and also created a transition into the first level.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import sys
 import pygame
+import pygame_menu
 
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Name_of_Game')
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Game Jam 1')
 
 running = True
 
@@ -15,8 +16,19 @@ def quit_screen():
     running = False
     sys.exit()
 
-while running:
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit_screen()
+def play_level_1():
+    global running
+    while running:
+        screen.fill((52, 152, 219))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_screen()
+
+
+menu = pygame_menu.Menu(300, 400, 'Space Invaders', theme=pygame_menu.themes.THEME_BLUE)
+menu.add_text_input('Name: ', default='John Doe')
+menu.add_button('Play', play_level_1)
+menu.add_button('Quit', quit_screen)
+
+menu.mainloop(screen)
